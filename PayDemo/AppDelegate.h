03d35a8327efa425b7f49ce8e5827fe64e277a33
lzh <lzh@cheok.com>
2018-08-09 14:58:10 +0800

    Initial Commit

---
+++ PayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PayDemo
+//
+//  Created by 李志华 on 2018/8/9.
+//  Copyright © 2018年 Chris Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayDemo
+//
+//  Created by 李志华 on 2018/8/9.
+//  Copyright © 2018年 Chris Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
